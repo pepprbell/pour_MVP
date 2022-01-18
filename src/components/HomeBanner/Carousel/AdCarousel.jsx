82b9eaea16eb3dadee961129ed5c7523e17fc16b
pepprbell <pepprbell@gmail.com>
2022-01-18 16:47:10 +0900

    Separate homepage banners to components

---
+++ src/components/HomeBanner/Carousel/AdCarousel.jsx
@@ -0,0 +1,17 @@
+import styles from './AdCarousel.module.css'
+import YouTube from 'react-youtube';
+import image from '../../../assets/dodesho.png'
+
+function AdCarousel(params) {
+    const videoId = ['dyw2TA1YlxM','Chgi1gCyLko']
+    const opts = {
+        height: '390',
+        width: '640'
+    }
+
+    return (
+        <div></div>
+    )
+}
+
+export default AdCarousel
