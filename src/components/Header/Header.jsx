b03937597caa820afd37dcf2843e90ebcf55aa17
pepprbell <pepprbell@gmail.com>
2022-01-18 16:49:08 +0900

    Separate header and slightly reformed it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,17 @@ import tommy from '../../assets/tommy.png'
 import signature from '../../assets/signature.png'
 
 function Header(props) {
+  const height = window.innerHeight - 15
+
   return (
-    <header id="main" className={styles.main} style={{top: props.topCss, visibility: props.audiovisibleCss}}>
+    <div id="main" className={styles.main} style={{height: height}}>
       <div className={styles.intro}>
         <h1>여기엔 인삿말<br/>저쪽엔 프사 ></h1>
         <audio id="voice" controls src={voice} className={styles.audio}></audio>
       </div>
       <img src={tommy} alt="" className={styles.tommy}/>
       <img src={signature} alt="" className={styles.sig}/>
-    </header>
+    </div>
   )
 }
 
