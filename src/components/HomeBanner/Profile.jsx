6b94abc55ca646325d7773346afd85b85fbf152d
pepprbell <pepprbell@gmail.com>
2022-01-27 23:21:06 +0900

    Make carousel (finally)

--- src/components/HomeBanner/Profile.jsx
+++ src/components/HomeBanner/Profile.jsx
@@ -5,13 +5,13 @@ function Profile(props) {
         <div>
             <div className={styles.profile}>
                 <div className={styles.left}>
-                <h1>이름</h1>
+                {/* <h1>이름</h1>
                 <div className={styles.line}></div>
                 <div>
                     <a className={styles.lov} href="https://lordofvoice.com/seungwoo" target='_blank'>로드오브보이스</a>
                     <a className={styles.insta} href="https://www.instagram.com/minswoosh/" target='_blank'>인스타그램</a>
                     <a className={styles.naver} href="https://cafe.naver.com/mvp1103" target='_blank'>네이버 카페</a>
-                </div>
+                </div> */}
                 </div>
             </div>
         </div>
