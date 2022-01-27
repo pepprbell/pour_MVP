880cbe370eade626c2b930d98c7ffe6423d03748
pepprbell <pepprbell@gmail.com>
2022-01-27 23:22:27 +0900

    etc

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,27 +2,13 @@ import React, { useState, useEffect } from "react"
 import styles from './index.module.css'
 import { useHistory } from "react-router-dom"
 import {
-    Card, Header, Profile, Footer, AdCarousel
+    Card, Header, Profile, Carousel
 } from '../../components'
 
 function Home(props) {
     const history = useHistory()
 
-    // 헤더 스크롤할때 효과
-    const [scroll, setScroll] = useState(0)
-    const [topCss, setTopCss] = useState(0)
-    const [visibleCss, setVisibleCss] = useState("visible")
-    // const [profilePos, setProfilePos] = useState("relative")
-    // const [profileTop, setProfileTop] = useState("140px")
-
-    const handleScroll = () => {
-    }
-
     useEffect(() => {
-    window.addEventListener('scroll', handleScroll, {passive: true})
-    return () => {
-        window.removeEventListener('scroll', handleScroll)
-    }
     }, [])
 
     // 스크롤 컨트롤
@@ -53,40 +39,28 @@ function Home(props) {
     // }
 
     return (
-        <div>
+        <div className={styles.container}>
             <Header></Header>
-            <div className={styles.blank}></div>
             <div className={styles.content}>
+            <div className={styles.blank}></div>
             <Profile></Profile>
             <div className={styles.rear}>
-                <div className={styles.profileSample}>
-                    <div className={styles.title}>
-                    <h1>게임</h1>
-                    <p>모두보기</p>
-                    </div>
-                    <div className={styles.cards}>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                    </div>
+                <div className={styles.cards}>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
                 </div>
-                <div className={styles.profileSample}>
-                    <div className={styles.title}>
-                    <h1>광고</h1>
-                    <p onClick={() => history.push({pathname:'/seeall', state:{type:'ad'}})}>모두보기</p>
-                    </div>
-                    <div className={styles.cards}>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                        <Card type="home"></Card>
-                    </div>
+                <Carousel></Carousel>
+                <div className={styles.cards}>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
+                    <Card type="home"></Card>
                 </div>
+                <Carousel content='etc'></Carousel>
             </div>
             </div>
-                <AdCarousel></AdCarousel>
-            <Footer></Footer>
         </div>
     )
 }
