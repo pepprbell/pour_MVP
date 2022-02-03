ced1a7932134b093ab75a7b84b8447540183cd21
pepprbell <pepprbell@gmail.com>
2022-02-03 21:11:47 +0900

    Deploy backend

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,11 +16,5 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
-    },
-    "storage": {
-        "s3mvpfanpagestoraged3go9z7ekbcnpt": {
-            "BucketName": "string",
-            "Region": "string"
-        }
     }
 }
