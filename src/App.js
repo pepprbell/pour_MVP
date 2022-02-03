9b0d37005f0c98db0d3be6cbcb9c411374b1cec3
pepprbell <pepprbell@gmail.com>
2022-02-03 21:16:45 +0900

    Edit App.js since navbar had changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 import styles from './App.module.css'
 import {
-  Home, SeeAll, Create
+  Home, SeeAll, Create,
 } from './pages'
 import {
-  MenuM, MenuD, Footer
+  MenuM, MenuD, Footer, Navbar
 } from './components'
 import './App.css'
 import { Route } from 'react-router-dom';
@@ -13,15 +13,15 @@ import { useState } from 'react';
 function App() {
   return (
     <div>
-      <MenuM visible={window.innerWidth>=800 ? "hidden" : "visible"}></MenuM>
-      <MenuD visible={window.innerWidth>=800 ? "visible" : "hidden"}></MenuD>
+      <MenuD></MenuD>
+      <MenuM></MenuM>
       <BrowserRouter>
         <Route exact path='/' component={ Home }></Route>
         <Route path='/seeall' component={ SeeAll }></Route>
         <Route path='/create' component={ Create }></Route>
         {/* <Route path='/' component={  }></Route> */}
       </BrowserRouter>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </div>
   )
 }
