9d40734d3099c740bd37b7e434fba243ac721bc1
pepprbell <pepprbell@gmail.com>
2022-02-18 16:16:01 +0900

    Adjust css

--- src/pages/Audiobook/index.jsx
+++ src/pages/Audiobook/index.jsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from 'react'
 import styles from './index.module.css'
-import none from '../../assets/none.png'
-import audiobook1 from '../../assets/audiobook1.png'
-import audiobook2 from '../../assets/audiobook2.png'
-import audiobook3 from '../../assets/audiobook3.png'
 // import audiobook from '../../assets/audiobook.png'
 // import audiobook from '../../assets/audiobook.png'
 import {
-    Navbar, Cards
+    Navbar, Cards, BookBanner
 } from '../../components'
 
 import { Audiobook } from '../../models'
@@ -42,19 +38,7 @@ function Audiobooks () {
         <div className={styles.container}>
             {/* <Navbar subKr={subKr} subEn={subEn}></Navbar> */}
             <div className={styles.content}>
-                <div className={styles.banner}>
-                    <div className={styles.side}>
-                        <img src={audiobook2} alt="" onClick={() => {goOut('https://audioclip.naver.com/audiobooks/C7F76AAC59')}} />
-                        <img src={none} alt="" onClick={() => {goOut('')}} />
-                    </div>
-                    <div className={styles.middle}>
-                        <img src={audiobook1} alt="" onClick={() => {goOut('https://audioclip.naver.com/audiobooks/B07205E56A')}} />
-                    </div>
-                    <div className={styles.side}>
-                        <img src={audiobook3} alt="" onClick={() => {goOut('https://audioclip.naver.com/audiobooks/34CB7A1B43')}} />
-                        <img src={none} alt="" onClick={() => {goOut('')}} />
-                    </div>
-                </div>
+                <BookBanner></BookBanner>
                 <div className={styles.cards}>
                     <Cards first='Book' second='Author' third='Date' query={audiobook}></Cards>
                 </div>
