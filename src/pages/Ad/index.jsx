a75637d5b84845cb056b6f9c2124be3e89d34471
pepprbell <pepprbell@gmail.com>
2022-02-18 16:04:38 +0900

    Update ad page

--- src/pages/Ad/index.jsx
+++ src/pages/Ad/index.jsx
@@ -8,34 +8,50 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 function Ads () {
-  const subKr = []
-  const subEn = []
+  const subKr = ['#해시태그1','#해시태그2','#해시태그3']
+  const subEn = ['hashtag1','hashtag2','hashtag3']
   const [current, setCurrent] = useState(0)
+  const [last, setLast] = useState(0)
 
-  const left = () => {
-    if (current !== 0 && true) {
-      setCurrent(current+436)
+  const getLast = () => {
+    if (window.innerWidth >= 1318) {
+      setLast(-436 * 1)
+      return
+    }
+    if (window.innerWidth >= 870) {
+      setLast(-436 * 2)
+      return
+    }
+    if (window.innerWidth < 436) {
+      setLast(-436 * 3)
     }
   }
 
+  useEffect(() => {
+    getLast()
+  }, [])
+
+  const left = () => {
+    setCurrent(current+436)
+  }
+
   const right = () => {
-    if (current !== 3) {
-      setCurrent(current-436)
-    }
+    setCurrent(current-436)
   }
 
   return(
     <div className={styles.container}>
-        <Navbar subKr={subKr} subEn={subEn}></Navbar>
-        <div className={styles.banner}></div>
-        <Carousel></Carousel>
+        <Navbar subKr={subKr} subEn={subEn} dark='123'></Navbar>
+        <div className={styles.banner}>
+          <Carousel></Carousel>
+        </div>
         <div className={styles.content}>
           <div className={styles.hash}>
             <div className={styles.center}>
-              <h1 className={styles.main}><a id='dubbing'></a>#Hashtag&nbsp;&nbsp; #hash</h1>
+              <h1 className={styles.main}><a id='hashtag1'></a>#Hashtag&nbsp;&nbsp; #hash</h1>
               <div className={styles.buttons}>
                 <button onClick={left} disabled={current === 0 ? true : false}><FontAwesomeIcon icon={faChevronLeft}/></button>
-                <button onClick={right} disabled={current === -436 ? true : false}><FontAwesomeIcon icon={faChevronRight}/></button>
+                <button onClick={right} disabled={current === last ? true : false}><FontAwesomeIcon icon={faChevronRight}/></button>
               </div>
             </div>
             <div className={styles.carContainer}>
@@ -43,15 +59,62 @@ function Ads () {
               <li className={classnames(styles.item)} style={{transform: "translate("+current+"px,0)"}}>
                 <VidCard></VidCard>
               </li>
-              {/* <li className={classnames(styles.item, styles.two)}> */}
               <li className={classnames(styles.item)} style={{transform: "translate("+(current+436)+"px,0)"}}>
                 <VidCard></VidCard>
               </li>
-              {/* <li className={classnames(styles.item, styles.three)}> */}
               <li className={classnames(styles.item)} style={{transform: "translate("+(current+872)+"px,0)"}}>
                 <VidCard></VidCard>
               </li>
-              {/* <li className={classnames(styles.item, styles.four)}> */}
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+1308)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div className={styles.hash}>
+            <div className={styles.center}>
+              <h1 className={styles.main}><a id='hashtag2'></a>#Hashtag&nbsp;&nbsp; #hash</h1>
+              <div className={styles.buttons}>
+                <button onClick={left} disabled={current === 0 ? true : false}><FontAwesomeIcon icon={faChevronLeft}/></button>
+                <button onClick={right} disabled={current === last ? true : false}><FontAwesomeIcon icon={faChevronRight}/></button>
+              </div>
+            </div>
+            <div className={styles.carContainer}>
+            <ul className={classnames(styles.items)}role="list">
+              <li className={classnames(styles.item)} style={{transform: "translate("+current+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+436)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+872)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+1308)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div className={styles.hash}>
+            <div className={styles.center}>
+              <h1 className={styles.main}><a id='hashtag3'></a>#Hashtag&nbsp;&nbsp; #hash</h1>
+              <div className={styles.buttons}>
+                <button onClick={left} disabled={current === 0 ? true : false}><FontAwesomeIcon icon={faChevronLeft}/></button>
+                <button onClick={right} disabled={current === last ? true : false}><FontAwesomeIcon icon={faChevronRight}/></button>
+              </div>
+            </div>
+            <div className={styles.carContainer}>
+            <ul className={classnames(styles.items)} role="list">
+              <li className={classnames(styles.item)} style={{transform: "translate("+current+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+436)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
+              <li className={classnames(styles.item)} style={{transform: "translate("+(current+872)+"px,0)"}}>
+                <VidCard></VidCard>
+              </li>
               <li className={classnames(styles.item)} style={{transform: "translate("+(current+1308)+"px,0)"}}>
                 <VidCard></VidCard>
               </li>
