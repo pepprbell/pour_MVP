fd6a3ca683a1efa1e7b36ffb0fa59158dc3ed349
pepprbell <pepprbell@gmail.com>
2022-02-18 22:02:28 +0900

    Remove cards components (lack of usage)

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,7 @@ import classnames from 'classnames';
 import { useState } from 'react'
 
 function Card(props) {
+  const query = props.query
   const none = "https://user-images.githubusercontent.com/67995526/154494368-d2a4d6e1-dd45-42b0-8d0e-22e6719bc2d0.png"
 
   const movePage = () => {
@@ -10,13 +11,13 @@ function Card(props) {
   }
   
   return (
-    <div className={classnames(styles.card, (props.type ? styles.homeCard : styles.allCard))} style={{visibility:props.visible}} onClick={movePage}>
+    <div className={classnames(styles.card, (props.type ? styles.homeCard : styles.allCard))} style={{visibility:props.visible}} onClick={movePage} key={query['id']} ref={props.refs}>
     <figure><img className={styles.img} src={props.query ? props.query.Image : none} alt="" /></figure>
     <div className={classnames(styles.white, (props.type ? styles.homeWhite : styles.allWhite))}>
       <div className={(props.type ? styles.homeDiv : styles.allDiv)}>
-        <h4 className={styles.first}>{props.first}</h4>
-        <p className={styles.second} href={props.href}>{props.second}</p>
-        <p className={styles.second}>{props.third}</p>
+        <h4 className={styles.first}>{query[props.first]}</h4>
+        <p className={styles.second} href={props.href}>{query[props.second]}</p>
+        <p className={styles.second}>{query[props.third]}</p>
       </div>
     </div>
   </div>
