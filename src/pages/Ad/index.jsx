a4ada020cf63d11a4c3e91123ed9c64976753b50
pepprbell <pepprbell@gmail.com>
2022-02-23 18:58:39 +0900

    Big changes to ad

--- src/pages/Ad/index.jsx
+++ src/pages/Ad/index.jsx
@@ -1,11 +1,41 @@
+import { useState, useRef, useCallback } from 'react'
 import styles from './index.module.css'
 import {
-    Navbar, Carousel, CardCarousel
+    Navbar, Carousel, CardCarousel, VidCard
 } from '../../components'
+import GetAd from '../../hooks/getAd'
 
 function Ads () {
-  const subKr = ['#해시태그1','#해시태그2','#해시태그3']
+  const subKr = ['','','']
   const subEn = ['hashtag1','hashtag2','hashtag3']
+  const [pageNumber, setPageNumber] = useState(0)
+
+  const {
+      ads,
+      hasMore,
+      loading,
+      error
+  } = GetAd(9, pageNumber)
+
+  const observer = useRef()
+  const lastAdElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+  
+  const [prev, setPrev] = useState('')
+  const stopAndRun = (data) => {
+    if (prev) {
+      prev.current.internalPlayer.pauseVideo()
+    }
+    setPrev(data)
+  }
 
   return(
     <div className={styles.container}>
@@ -14,8 +44,18 @@ function Ads () {
         <Carousel></Carousel>
       </div>
       <div className={styles.content}>
-        <CardCarousel></CardCarousel>
+        <div className={styles.cards}>
+          {ads.map((ad,index) => {
+            if (ads.length === index+1) {
+              return <VidCard refs={lastAdElementRef} query={ad} first='Ad' second='Date' isPlaying={stopAndRun}></VidCard>
+            } else {
+              return <VidCard query={ad} first='Ad' second='Date' isPlaying={stopAndRun}></VidCard>
+            }
+          })}
+        </div>
       </div>
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </div>
   )
 }
