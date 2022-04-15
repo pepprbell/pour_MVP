0bb7d28be4abfdd3bf94a16856bfdc39b199a9a5
pepprbell <pepprbell@gmail.com>
2022-04-16 00:21:21 +0900

    Remove input component and modify select

--- src/components/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Input.module.css'
-
-function Input(props) {
-    return(
-        <input className={styles.input} type={props.type} placeholder={props.placeholder} onChange={props.inputHandler}/>
-    )
-}
-
-export default Input
