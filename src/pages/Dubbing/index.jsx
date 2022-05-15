b43e3bff275d2621d87a23eaf3ed5bff2cd21e72
pepprbell <pepprbell@gmail.com>
2022-05-15 23:02:07 +0900

    Make frame for dubbing pages

--- src/pages/Dubbing/index.jsx
+++ src/pages/Dubbing/index.jsx
@@ -1,52 +1,40 @@
-import { useState, useRef, useCallback } from 'react'
 import styles from './index.module.css'
 import {
-    Card, BookBanner, Navbar
+    Navbar,
 } from '../../components'
-import GetGame from '../../hooks/getGame'
+import { Game, TVAnime, Theater } from '../'
 
-function Dubbings () {
-  const [pageNumber, setPageNumber] = useState(0)
-  const subKr = ['게임']
-  const subEn = ['game']
+function Dubbings() {
+    const sub = [['게임','game',true],['TV 애니메이션','tva',false],['극장 애니메이션','theater',false]]
 
-  const {
-    games,
-    hasMore,
-    loading,
-    error
-  } = GetGame(30, pageNumber)
-
-  const observer = useRef()
-  const lastGameElementRef = useCallback(node => {
-    if (loading) return
-    if (observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMore) {
-        setPageNumber(prevPageNumber => prevPageNumber + 1)
+    const WhichOne = () => {
+      const hash = window.location.hash
+      if (hash === '#game') {
+        return (
+          <>
+            <Game></Game>
+          </>
+        )
       }
-    })
-    if (node) observer.current.observe(node)
-  }, [loading, hasMore])
-
-  return (
-    <div className={styles.container}>
-      <Navbar subKr={subKr} subEn={subEn}></Navbar>
-      <div className={styles.content}>
-        <div className={styles.cards}>
-          {games.map((game,index) => {
-            if (games.length === index+1) {
-              return <Card refs={lastGameElementRef} query={game} first='Character' second='Content'></Card>
-            } else {
-              return <Card query={game} first='Character' second='Content'></Card>
-            }
-          })}
-        </div>
-        <div>{loading && 'Loading...'}</div>
-        <div>{error && 'Error'}</div>
+      // if (hash === '#') {
+      //   return (
+      //     <>
+      //     </>
+      //   )
+      // }
+      return (
+        <>
+          <Game></Game>
+        </>
+      )
+    }
+    
+    return(
+      <div>
+        <Navbar sub={sub}></Navbar>
+        <WhichOne></WhichOne>
       </div>
-    </div>
-  )
+    )
 }
 
 export default Dubbings
